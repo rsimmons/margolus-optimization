c4d5076451932957afb44413a63b15e9fcbe77f8
Russel Simmons <symstym@mac.com>
2014-02-05 17:03:15 -0800

    remove obsolete comments

--- v1/__init__.py
+++ v1/__init__.py
@@ -5,6 +5,5 @@ import subproc_util
 description = 'C impl, one bit per cell, fancy code-gen even/odd inner loops handling 64 cells with 16-entry shift-based rule lookup'
 
 def evolve(state, rule, iters):
-    # TODO: make use of rule
     return subproc_util.evolve_block_64_cgen(os.path.join(os.path.dirname(__file__), 'evolve_bin'), state, rule, iters)
 
--- v5/__init__.py
+++ v5/__init__.py
@@ -5,6 +5,5 @@ import subproc_util
 description = 'C impl, one bit per cell, fancy code-gen even/odd inner loops handling 64 cells with 16-entry array rule lookup. better edge wrap handling on odd phase. (based on v3)'
 
 def evolve(state, rule, iters):
-    # TODO: make use of rule
     return subproc_util.evolve_block_64_cgen(os.path.join(os.path.dirname(__file__), 'evolve_bin'), state, rule, iters)
 
