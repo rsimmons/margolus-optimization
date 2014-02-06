620b119cd555bbe0ba9c4271386d0aba9e810275
Russel Simmons <symstym@mac.com>
2014-02-05 17:57:10 -0800

    update description

--- v6/__init__.py
+++ v6/__init__.py
@@ -2,7 +2,7 @@ import os
 
 import subproc_util
 
-description = 'C impl, one bit per cell, fancy code-gen even/odd inner loops handling 64 cells with 16-entry array rule lookup. better edge wrap handling on odd phase. (based on v3)'
+description = 'C impl, one bit per cell, fancy code-gen even-only inner loop handling 64 cells with 16-entry array rule lookup. (based on v5)'
 
 def evolve(state, rule, iters):
     return subproc_util.evolve_block_64_cgen(os.path.join(os.path.dirname(__file__), 'evolve_bin'), state, rule, iters)
